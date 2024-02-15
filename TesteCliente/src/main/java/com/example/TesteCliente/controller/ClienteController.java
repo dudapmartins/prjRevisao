e3f0a0ed9cb9b0f439d7cd9f2821deb6ccd53b5c
dudapmartins <133990775+dudapmartins@users.noreply.github.com>
2024-02-15 14:17:31 -0300

    Add files via upload

--- TesteCliente/src/main/java/com/example/TesteCliente/controller/ClienteController.java
+++ TesteCliente/src/main/java/com/example/TesteCliente/controller/ClienteController.java
@@ -2,22 +2,17 @@ package com.example.TesteCliente.controller;
 
 import java.util.List;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.MariaEduardaPedroza.PrjEmpresa.entities.Departamento;
 import com.example.TesteCliente.entities.Cliente;
 import com.example.TesteCliente.service.ClienteService;
-
 
 @RestController
 @RequestMapping("/cliente")
@@ -34,32 +29,23 @@ public class ClienteController {
 	}
 
 	@GetMapping("/{id}")
-	public ResponseEntity<Cliente> getCliente(@PathVariable Long depCodigo) {
-		Cliente cliente = clienteService.getClienteById(depCodigo);
+	public ResponseEntity<Cliente> getCliente(@PathVariable Long clienteID) {
+		Cliente cliente = clienteService.getClienteById(clienteID);
 		if (cliente != null) {
 			return ResponseEntity.ok(cliente);
 		} else {
 			return ResponseEntity.notFound().build();
 		}
 	}
-	@GetMapping("/home")
-	public String paginaInicial() {
-		return "index";// nome do seu arquivo HTML (sem a extensão)
-	}
 
 	@DeleteMapping("/{id}")
-	public void deleteCliente(@PathVariable Long depCodigo) {
-		clienteService.deleteCliente(depCodigo);
+	public void deleteCliente(@PathVariable Long clienteID) {
+		clienteService.deleteCliente(clienteID);
 	}
 
-	// Utilizando o ResponseEntity e RequestEntity
 	@GetMapping
-	public ResponseEntity<List<Cliente>> getAllCliente(RequestEntity<Void> requestEntity) {
-		String method = requestEntity.getMethod().name();
-		String contentType = requestEntity.getHeaders().getContentType().toString();
-		List<Cliente> cliente = clienteService.getAllCliente();
-		return ResponseEntity.status(HttpStatus.OK).header("Method", method).header("Content-Type", contentType)
-				.body(cliente);
+	public List<Cliente> getAllCliente() {
+		return clienteService.getAllCliente();
 	}
 
 }
